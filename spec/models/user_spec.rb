93f2f1676ce6c2a11adb3df17d3b501284d9ceba
Adrian Cunanan <adrian@adriancunanan.com>
2014-08-22 16:16:02 -0400

    Make a basic User model (including secure passwords)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -59,15 +60,14 @@ describe User do
       user_with_same_email.email = @user.email.upcase
       user_with_same_email.save
     end
+
     it { should_not be_valid }
   end
 
-  it { should respond_to(:authenticate) }
-
   describe "when password is not present" do
     before do
-      @user = User.new(name: "Example User", email: "user@example.com",
-                       password: " ", password_confirmation: " ")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: " ", password_confirmation: " ")
     end
     it { should_not be_valid }
   end
